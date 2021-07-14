6ce1435270b14340be51e87b18f42e6c72ef0f3f
MisterSquishy <pdavids@comcast.net>
2021-07-14 09:37:26 -0400

    animate squirrel

--- src/components/Ground/index.js
+++ src/components/Ground/index.js
@@ -1,14 +1,49 @@
-import React, { useRef } from 'react'
+import React, { useCallback, useRef } from 'react'
 
-import squirrel from '../../illustrations/squirrel.png'
+import squirrel1 from '../../illustrations/squirrel/squrrel1.png'
+import squirrel2 from '../../illustrations/squirrel/squrrel2.png'
+import squirrel3 from '../../illustrations/squirrel/squrrel3.png'
+import squirrel4 from '../../illustrations/squirrel/squrrel4.png'
+import squirrel5 from '../../illustrations/squirrel/squrrel5.png'
 
 import './style.css'
 
+const SQUIRRELS = [
+  squirrel1,
+  squirrel2,
+  squirrel3,
+  squirrel4,
+  squirrel5
+]
+
 const Ground = () => {
   const groundCanvasRef = useRef()
+
+  const animate = useCallback((frame, progress) => {
+    if (groundCanvasRef.current) {
+      const squirrel = new Image();
+      squirrel.src = SQUIRRELS[frame % SQUIRRELS.length]
+      const context = groundCanvasRef.current.getContext('2d'); 
+      context.clearRect(0, 0, groundCanvasRef.current.width, groundCanvasRef.current.height)
+      context.drawImage(squirrel, groundCanvasRef.current.width - progress, 0);
+    }
+  }, [groundCanvasRef])
+
+  let frame = 0
+  let progress = 0
+  setInterval(() => {
+    animate(frame, progress)
+    frame++
+    if (progress > window.innerWidth + 400) {
+      progress = 0
+    } else {
+      progress = progress + 10
+    }
+  }, 100);
+
+
   return <div className='ground'>
-      <canvas ref={groundCanvasRef} />
-      <img src={squirrel} alt="squirrel" width="80px" />
+      <canvas ref={groundCanvasRef} width={window.innerWidth} height="500px" />
     </div>
 }
 
